ea11de19aebffa4c1ca4dbe3fdefbdd72c3d323d
Tyler Wilson <tyler_wilson@yahoo.com>
2014-08-24 11:42:11 -0400

    Programming Assignment for Coursera R Programming
    This is an example of a user defined function to create a cached
    matrix, and then create the inverse of that matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Builds a function to compute the inverse of a matrix.
 
-## Write a short comment describing this function
+## makeCacheMatrix is a function that will create a matrix 
+## that will be used to "solve" for the inverse of that matrix.
 
-makeCacheMatrix <- function(x = matrix()) {
+## This assignment assumes that the matrix supplied will always 
+## be invertible.
 
+## makeCacheMatrix saves the matrix to variable mat.
+## The inverse of the variable mat will be variable inv.
+## The method is to first cache a matrix that creates a list to
+## set the value of the matrix and get the value of the matrix.
+## These values will be used in the cacheSolve function
+## to solve the inverse of the matrix that was created.
+
+makeCacheMatrix <- function(mat = matrix()) {
+        inv <- NULL
+        set <- function (y) {
+                mat <<- y
+                inv <<- NULL
+        }
+        get <- function() mat
+        setmatrix <- function(solve) inv <<- solve
+        getmatrix <- function () inv
+        list(set = set, get = get,
+             setmatrix = setmatrix,
+             getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is a user function that will solve for the 
+## inverse of the matrix saved in makeCacheMatrix.  The
+## variables used in this function were created with the
+## makeCacheMatrix user function.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mat = matrix(), ...) {
+        inv <- mat$getmatrix()
+        if(!is.null(inv)) {
+                message("Getting cached data")
+                return(inv)
+        }
+        matrix <- mat$get()
+        inv <- solve(matrix, ...)
+        mat$setmatrix(inv)
+        inv
 }
